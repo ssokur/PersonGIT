be663a3f5db2dcee0b7ef29e86c94978c026d9f4
ArtemGitHubConnection <romanenkoartem84@gmail.com>
2014-11-20 16:38:36 +0200

    Artem РАБОЧИЙ!!!!

---
+++ src/Ener/App_Main.java
@@ -0,0 +1,12 @@
+package src.Ener;
+
+/**
+ * Created by Artem on 20.11.14.
+ */
+public class App_Main
+{
+    public static void main(String[] args)
+    {
+        new GUI.Frame_Person();
+    }
+}
