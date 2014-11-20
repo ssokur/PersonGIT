2d664ceaea0305a178137896bf40cebcc05d16cc
ssokur <s.sokur@mail.ru>
2014-11-20 17:49:39 +0200

    Interface + Person clsaa add

--- src/API/CRUD_Function.java
+++ src/API/CRUD_Function.java
@@ -1,9 +1,15 @@
 package src.API;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public interface CRUD_Function
 {
-    void create();
-    String read();
-    void update();
-    void delete();
+    void create(Person p) throws SQLException, ClassNotFoundException;
+
+    ArrayList read() throws SQLException, ClassNotFoundException;
+
+    void delete(Person p) throws SQLException, ClassNotFoundException;
+
+    void update(Person p) throws SQLException, ClassNotFoundException;
 }
