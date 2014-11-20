7bdb117b476292e17d916317f91ac97552a780ab
ssokur <s.sokur@mail.ru>
2014-11-20 16:14:47 +0200

    Тест

---
+++ GUI/Main_Pers.java
@@ -0,0 +1,12 @@
+package GUI;
+
+class Main_Person
+{
+    public static void main(String[] args)
+    {
+//        new API.DAL_Person();
+        new GUI.Frame_Person();
+
+        // test commit
+    }
+}
