3e03060842df0f2b25824e4a45de4daad07b4ee1
ArtemGitHubConnection <romanenkoartem84@gmail.com>
2014-11-20 14:07:19 +0200

    Artem Это набросок посмотрите если че не так поправим!!!!

--- src/GUI/CAppFrame.java
+++
@@ -1,9 +0,0 @@
-package GUI;
-
-import javax.swing.*;
-
-public class CAppFrame extends JFrame
-
-{
-    // main frame
-}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import API.DAL_Pers;
+import GUI.Frame_Pers;
+
 public class Main
 {
     public static void main(String[] args)
     {
-
+        new DAL_Pers();
+        new Frame_Pers();
     }
 }
