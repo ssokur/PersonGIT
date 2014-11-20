45944d78f867f7f58300f943f234a907bc061ab3
ssokur <s.sokur@mail.ru>
2014-11-20 18:05:08 +0200

    DAL Person -

--- src/API/DAL_Person.java
+++ src/API/DAL_Person.java
@@ -1,13 +1,34 @@
 package src.API;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class DAL_Person
 {
+    DAL_Person pd = null;
+
     public DAL_Person()
     {
-        new DAO_SQL();
-        new DAO_Mongo();
-        new DAO_JSON();
-        new DAO_XML();
+//        new DAO_SQL();
+//        new DAO_Mongo();
+//        new DAO_JSON();
+//        new DAO_XML();
+    }
+
+    void create(Person p) throws SQLException, ClassNotFoundException {
+        pd.create(p);
+    }
+
+    public ArrayList<Person> read() throws SQLException, ClassNotFoundException {
+        return pd.read();
+    }
+
+    void delete(Person p) throws SQLException, ClassNotFoundException {
+        pd.delete(p);
+    }
+
+    void update(Person p) throws SQLException, ClassNotFoundException {
+        pd.update(p);
     }
 
 }
