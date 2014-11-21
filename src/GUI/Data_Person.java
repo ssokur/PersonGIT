ddf351038e15b454a3b91217d5a20bbf53f015d1
ArtemGitHubConnection <romanenkoartem84@gmail.com>
2014-11-21 20:03:51 +0200

    Artem Make good face of classes

--- src/GUI/Data_Person.java
+++ src/GUI/Data_Person.java
@@ -1,16 +1,31 @@
 package src.GUI;
 
+import src.API.Person;
 import src.Locale.Lockale;
+
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Data_Person
 {
     public ResourceBundle res = null;
     public static ToolBar toolBar = null;
+    public ArrayList<Person> current = null;
 
     public Data_Person()
     {
+
         new Lockale();
+    }
+
+    public void initLocale()
+    {
         res = Lockale.initBundle();
     }
+
+    public void initList(ArrayList<Person> currentIn)
+    {
+        current = currentIn;
+    }
+
 }
---
+++ src/GUI/LeftPanel.java
@@ -0,0 +1,14 @@
+package src.GUI;
+
+import javax.swing.*;
+
+/**
+ * Created by Artem on 21.11.14.
+ */
+public class LeftPanel extends JPanel
+{
+    public LeftPanel()
+    {
+
+    }
+}
