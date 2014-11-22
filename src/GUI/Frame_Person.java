2c36b15c40006c780344fcfa1d8251965927f7ec
ssokur <s.sokur@mail.ru>
2014-11-22 14:37:01 +0200

    Chance GUI

--- src/GUI/Frame_Person.java
+++ src/GUI/Frame_Person.java
@@ -32,7 +32,7 @@ public class Frame_Person extends JFrame
 
         add(tablePerson, BorderLayout.CENTER);      // add panel with table data
         add(Pbtn, BorderLayout.AFTER_LAST_LINE);    // add panel with buttons
-        add(tBar, BorderLayout.BEFORE_FIRST_LINE);
+        add(tBar, BorderLayout.BEFORE_FIRST_LINE);  // add panel with toolBar
 
         setJMenuBar(menu);
         setPreferredSize(new Dimension(600, 600));
