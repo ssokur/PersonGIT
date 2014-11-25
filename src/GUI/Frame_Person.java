c3c590c4d64396ee5a2d217a938e16d1e9d6a06d
ssokur <s.sokur@mail.ru>
2014-11-25 22:52:10 +0200

    Add exception on many classes

--- src/GUI/Frame_Person.java
+++ src/GUI/Frame_Person.java
@@ -10,14 +10,14 @@ import java.text.ParseException;
 
 public class Frame_Person extends JFrame
 {
-    Main_Menu menu;         // init main mane
-    ToolBar tBar;           // init toolbar
-    StatBar stBar;          // init Status Bar
-    Commands comands;       // init class with commands
-    Data_Person data;       // init class with structure Person ArrayList
-    PanelButton pbtn;       // init panel for 4 buttons
-    DataModel dataModel;    // init class with ArrayList structure
-    PanelWork tablePerson;  // init table for data
+    Main_Menu       menu;         // init main mane
+    ToolBar         tBar;         // init toolbar
+    StatBar         stBar;        // init Status Bar
+    Commands        comands;      // init class with commands
+    Data_Person     data;         // init class with structure Person ArrayList
+    PanelButton     pbtn;         // init panel for 4 buttons
+    DataModel       dataModel;    // init class with ArrayList structure
+    PanelWork       tablePerson;  // init table for data
 
     public Frame_Person() throws ParseException, UnknownHostException
     {
