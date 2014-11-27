159e26f25db94a46af4fec377d35e585f250c57b
ssokur <s.sokur@mail.ru>
2014-11-27 21:29:31 +0200

    DAO_Hibernate Factory class HibernateUtil class

--- src/API/DAO_Hibernate.java
+++ src/API/DAO_Hibernate.java
@@ -1,8 +1,11 @@
 package src.API;
 
+import org.hibernate.classic.Session;
+
 import javax.swing.*;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class DAO_Hibernate implements CRUD_Function
 
@@ -10,13 +13,14 @@ public class DAO_Hibernate implements CRUD_Function
     @Override
     public void create(Person p) throws SQLException, ClassNotFoundException
     {
-        org.hibernate.classic.Session session = null;
+        Session session = null;
         try
         {
             session = HibernateUtil.getSessionFactory().openSession();
             session.beginTransaction();
-//            session.save(Person);
+            session.save(p);
 //            session.getTransaction().commit();
+
         } catch (Exception e)
         {
             JOptionPane.showMessageDialog(null, e.getMessage(), "Ошибка I/O", JOptionPane.OK_OPTION);
@@ -32,18 +36,58 @@ public class DAO_Hibernate implements CRUD_Function
     @Override
     public ArrayList read() throws SQLException, ClassNotFoundException
     {
-        return null;
+        Session session = null;
+        List<Person> pers = new ArrayList<Person>();
+//        ArrayList<Person> pers = new ArrayList<Person>();
+        try
+        {
+            session = HibernateUtil.getSessionFactory().openSession();
+            pers = session.createCriteria(Person.class).list();
+        } catch (Exception e)
+        {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Оќшибка I/O", JOptionPane.OK_OPTION);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+
+        return (ArrayList) pers;
     }
 
     @Override
-    public void delete(Person p) throws SQLException, ClassNotFoundException
-    {
-
+    public void delete(Person p) throws SQLException, ClassNotFoundException {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.delete(p);
+//            session.getTransaction().commit();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Ошибка I/O", JOptionPane.OK_OPTION);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public void update(Person p) throws SQLException, ClassNotFoundException
     {
-
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.update(p);
+//            session.getTransaction().commit();
+        } catch (Exception e)
+        {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Ошибка I/O", JOptionPane.OK_OPTION);
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
     }
 }
